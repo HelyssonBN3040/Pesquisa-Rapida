4abd30092c8e670f2c486affb7615420a78d7612
HelyssonBN3040 <helyssoncavalcante.com@gmail.com>
2023-11-28 13:23:45 -0300

    correção arrow

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-//HOME.JS
-
-//Global Variables
-var click = 0;
-
-//Post card dropdown
-$(".more").click(function(){
-    if (click == 0){ 
-        $(this).next(".dropdown_content").show(); click++;
-    } else {
-        $(this).next(".dropdown_content").hide(); click = 0;
-    }
-});
-
-$(document).click(function(){
-    if (click > 0) { $(".dropdown_content").hide(); click = 0; }
-});
-
-$(".more").click(function(e){
-    e.stopPropagation();
-});
-
+    
 
 //Cookies e Privacidade
 var msgCookies = document.getElementById('cookies-msg');
